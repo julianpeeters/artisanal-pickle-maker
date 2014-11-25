67e28decf16ebce5fdcf993b3adb3969f2d6600f
Julian Peeters <julianpeeters@gmail.com>
2014-11-24 23:01:06 -0800

    added alldatatypes test

--- src/test/scala/AllDatatypesSpec.scala
+++ src/test/scala/AllDatatypesSpec.scala
@@ -11,8 +11,13 @@ import scala.tools.scalap.scalax.rules.scalasig._
 import com.novus.salat.annotations.util._
 import scala.reflect.ScalaSignature
 
-class ByteSpec extends mutable.Specification {
-  val mySig = new ScalaSig(List("case class"), List("models", "MyRecord_Byte"), List(("a", "Byte")))
+
+
+class AllDatatypesSpec extends mutable.Specification {
+
+  val mySig = new ScalaSig(List("case class"), List("models", "MyRecord_AllDatatypes"), List(("a", "Byte"), ("b", "Short"), ("c", "Int"), ("d", "Long"), ("e", "Float"), ("f", "Double"), ("g", "Char"), ("h", "String"), ("i", "Boolean"), ("j", "Unit"), ("k", "Null"), ("l", "Nothing"), ("m", "Any"), ("n", "Byte"), ("o", "Object")))
+
+
   def parseByteCodeFromAnnotation(clazz: Class[_]): Option[ByteCode] = {
     clazz.annotation[ScalaSignature] match {
       case Some(sig) if sig != null => {
@@ -30,14 +35,17 @@ class ByteSpec extends mutable.Specification {
     Option(ByteCode(bytes.take(len)))   
   }
 
-
-  "a ScalaSig for case class MyRecord_Byte(a: Byte)" should {
+  "a ScalaSig for a case class with all supported datatypes (MyRecord_AllDatatypes) " should {
     "have the correct string" in {
-    val correctParsedSig = parseByteCodeFromAnnotation(classOf[MyRecord_Byte]).map(ScalaSigAttributeParsers.parse(_)).get
+
+
+ val correctParsedSig = parseByteCodeFromAnnotation(classOf[MyRecord_AllDatatypes]).map(ScalaSigAttributeParsers.parse(_)).get
     val myParsedSig = parseByteCodeFromMySig(mySig).map(ScalaSigAttributeParsers.parse(_)).get
- 
+
     correctParsedSig.toString === myParsedSig.toString
-    }
   }
 
+    }
+
 }
+
