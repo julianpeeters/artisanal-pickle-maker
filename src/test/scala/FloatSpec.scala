3c1430c49cbd4035e64bad009e9c63f5e291ff1f
Julian Peeters <julianpeeters@gmail.com>
2014-11-24 23:36:54 -0800

    why does more than one test class fail?

--- src/test/scala/FloatSpec.scala
+++ src/test/scala/FloatSpec.scala
@@ -1,23 +1,17 @@
 package artisanal.pickle.maker 
 import models._
 
-//import org.specs2._
-//import mutable._
-//import specification._
+import org.specs2._
+import mutable._
+import specification._
 
 import scala.reflect.internal.pickling.ByteCodecs
 import scala.tools.scalap.scalax.rules.scalasig._
 
 import com.novus.salat.annotations.util._
 import scala.reflect.ScalaSignature
-
-import collection.mutable.Stack
-import org.scalatest._
-
-
-class UserDefinedTypeSpec extends FlatSpec with Matchers {
-//class UserDefinedTypeSpec extends mutable.Specification {
-  val mySig = new ScalaSig(List("case class"), List("models", "MyRecord_User"), List(("myRec", "MyRec")))
+class FloatSpec extends mutable.Specification {
+  val mySig = new ScalaSig(List("case class"), List("models", "MyRecord_Float"), List(("e", "Float")))
   def parseByteCodeFromAnnotation(clazz: Class[_]): Option[ByteCode] = {
     clazz.annotation[ScalaSignature] match {
       case Some(sig) if sig != null => {
@@ -32,17 +26,17 @@ class UserDefinedTypeSpec extends FlatSpec with Matchers {
   def parseByteCodeFromMySig(sig: ScalaSig): Option[ByteCode] = {
     val bytes = sig.bytes.getBytes("UTF-8")
     val len = ByteCodecs.decode(bytes)
-    Option(ByteCode(bytes.take(len)))   
+    Option(ByteCode(bytes.take(len)))
   }
 
-  "a ScalaSig for case class MyRecord_User(myRec: MyRec)" should //{
+
+  "a ScalaSig for case class MyRecord_Float(e: Float)" should {
     "have the correct string" in {
-    val correctParsedSig = parseByteCodeFromAnnotation(classOf[MyRecord_User]).map(ScalaSigAttributeParsers.parse(_)).get
+    val correctParsedSig = parseByteCodeFromAnnotation(classOf[MyRecord_Float]).map(ScalaSigAttributeParsers.parse(_)).get
     val myParsedSig = parseByteCodeFromMySig(mySig).map(ScalaSigAttributeParsers.parse(_)).get
-//    correctParsedSig.toString === myParsedSig.toString
-    correctParsedSig.toString should be (myParsedSig.toString)
+ 
+    correctParsedSig.toString === myParsedSig.toString
     }
- // }
+  }
 
 }
-
