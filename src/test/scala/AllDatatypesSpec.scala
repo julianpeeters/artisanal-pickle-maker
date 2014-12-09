5d5432ea79dda187571c5840d8633e88b742ce2d
Julian Peeters <julianpeeters@gmail.com>
2014-12-08 23:00:06 -0800

    testing travis again

--- src/test/scala/AllDatatypesSpec.scala
+++ src/test/scala/AllDatatypesSpec.scala
@@ -20,7 +20,7 @@ class AllDatatypesSpec extends mutable.Specification {
   "a ScalaSig for a case class with all supported datatypes (MyRecord_AllDatatypes) " should {
     "have the correct string" in {
 
-      val mySig = new ScalaSig(List("case class"), List("models", "MyRecord_AllDatatypes"), List(("a", "Byte"), ("b", "Short"), ("c", "Int"), ("d", "Long"), ("e", "Float"), ("f", "Double"), ("g", "Char"), ("h", "String"), ("i", "Boolean"), ("j", "Unit"), ("k", "Null"), ("l", "Nothing"), ("m", "Any"), ("n", "Byte"), ("o", "Object")))
+      val mySig = new artisanal.pickle.maker.ScalaSig(List("case class"), List("models", "MyRecord_AllDatatypes"), List(("a", "Byte"), ("b", "Short"), ("c", "Int"), ("d", "Long"), ("e", "Float"), ("f", "Double"), ("g", "Char"), ("h", "String"), ("i", "Boolean"), ("j", "Unit"), ("k", "Null"), ("l", "Nothing"), ("m", "Any"), ("n", "Byte"), ("o", "Object")))
 
       val correctParsedSig = SigParserHelper.parseByteCodeFromAnnotation(classOf[MyRecord_AllDatatypes]).map(ScalaSigAttributeParsers.parse(_)).get
       val myParsedSig = SigParserHelper.parseByteCodeFromMySig(mySig).map(ScalaSigAttributeParsers.parse(_)).get
