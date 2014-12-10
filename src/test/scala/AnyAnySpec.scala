4168d9b546d2c0e3c68679b9339549b25d348295
Julian Peeters <julianpeeters@gmail.com>
2014-12-09 19:59:30 -0800

    testing travis

--- src/test/scala/AnyAnySpec.scala
+++ src/test/scala/AnyAnySpec.scala
@@ -19,7 +19,7 @@ class AnyAnySpec extends mutable.Specification {
   "a ScalaSig for case class MyRecord_AnyAny(m1: Any, m2: Any)" should {
     "have the correct string" in {
   val mySig = new ScalaSig(List("case class"), List("models", "MyRecord_AnyAny"), List(("m1", "Any"), ("m2", "Any")))
-println(mySig)
+println("anyspec sig: " + mySig)
    // val correctParsedSig = SigParserHelper.parseByteCodeFromAnnotation(classOf[MyRecord_AnyAny]).map(ScalaSigAttributeParsers.parse(_)).get
     val myParsedSig = SigParserHelper.parseByteCodeFromMySig(mySig)//.map(ScalaSigAttributeParsers.parse(_)).get
  
